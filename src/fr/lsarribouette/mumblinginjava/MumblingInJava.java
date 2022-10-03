15117d4a2270fa9e037d796995b1d1d2431ddb99
Lauranne Sarribouette <lauranne.sarribouette@gmail.com>
2022-10-03 17:54:38 +0200

    J'ai la position du joueur et le nom de son voisin de droite

--- src/fr/lsarribouette/mumblinginjava/MumblingInJava.java
+++ src/fr/lsarribouette/mumblinginjava/MumblingInJava.java
@@ -9,6 +9,45 @@ public class MumblingInJava {
 	
 	public static void main(String[] args) {
 		
+		String prenomJoueur;
+		String[][] classePositions = new String[DIM1][DIM2];
+		// je remplis mon tableau (sans les distanciels)
+		 classePositions[0][0] = "Thibault";
+		 classePositions[0][1] = "Quentin";
+		 classePositions[0][2] = "Pierrick";
+		 classePositions[0][3] = "Laura";
+		 classePositions[0][4] = "";
+		 classePositions[0][5] = "Hélène";
+		 classePositions[0][6] = "Déana";
+		 classePositions[0][7] = "Xavier F";
+		 classePositions[1][0] = "Akim";
+		 classePositions[1][1] = "Thomas";
+		 classePositions[1][2] = "";
+		 classePositions[1][3] = "";
+		 classePositions[1][4] = "";
+		 classePositions[1][5] = "Marie";
+		 classePositions[1][6] = "Lauranne";
+		 classePositions[1][7] = "Bruno";
+		 classePositions[2][0] = "Bastien";
+		 classePositions[2][1] = "Thibaut";
+		 classePositions[2][2] = "";
+		 classePositions[2][3] = "Mickaël";
+		 classePositions[2][4] = "";
+		 classePositions[2][5] = "Jonathan";
+		 classePositions[2][6] = "Lucie";
+		 classePositions[2][7] = "";
+		 classePositions[3][0] = "";
+		 classePositions[3][1] = "Théo";
+		 classePositions[3][2] = "Kantin";
+		 classePositions[3][3] = "Romain";
+		 classePositions[3][4] = "";
+		 classePositions[3][5] = "Nicolas";
+		 classePositions[3][6] = "Xavier A";
+		 classePositions[3][7] = "";
+		int ligne = -1;
+		int colonne = -1;
+		String voisinDroite;
+		String voirPlanClasse;
 		
 		Scanner console = new Scanner(System.in);
 		
@@ -24,8 +63,49 @@ public class MumblingInJava {
 		System.out.println("(!) Il y a \"Xavier F\" et \"Xavier A\"");
 		System.out.println();
 		
-		afficheClasse();
-
+		//Saisie
+		System.out.println("Prêt ? Alors commence par me donner ton prénom :");
+		//prenomJoueur = console.nextLine();
+		prenomJoueur = "Thomas";
+		
+		//Répéter jusqu'à avoir un nom d'élève
+		do {
+			//Position du joueur
+			for (int i=0; i < DIM1; i++) {
+				for (int j=0; j < DIM2; j++) {
+					if(prenomJoueur == classePositions[i][j]) {
+						ligne = i;
+						colonne = j;
+						System.out.println("la ligne est " + ligne + " et la colonne est " + colonne);
+						}
+					}
+				}
+		} while (ligne == -1);
+		
+		//Cherche voisin de droite (i; j+1)
+		if (colonne < DIM2-1) {
+			voisinDroite = classePositions[ligne][colonne+1];
+			if (classePositions[ligne][colonne+1] == "") {
+				System.out.println("Tu as un vide à ta droite.");
+			} else {
+				System.out.println("Ton voisin à droite est "+voisinDroite);
+			}
+		} else {
+			System.out.println("Tu n'as pas de voisin à droite.");
+		}
+		
+		
+		
+		//Choix d'afficher la classe
+		System.out.println("Si tu souhaites afficher le plan de classe, saisis \"oui\". " 
+				+ "Sinon, merci d'avoir joué ! :)");
+		voirPlanClasse = console.nextLine();
+		if (voirPlanClasse.equals("oui")) {
+			afficheClasse();
+		}
+		
+		console.close();
+		
 	}
 	public static void afficheClasse() {
 		String[][] classe = new String[DIM1][DIM2];
